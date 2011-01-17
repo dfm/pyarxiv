ffdde87483fdffec194cfe241ea77792fae640a3
Dan F-M <dan@danfm.ca>
2011-01-16 23:12:04 -0500

    stupid comparison of papers from a bibtex library based on cosine angle

---
+++ bibparser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+bibparser.py
+
+Created by Dan F-M on 2011-01-16.
+"""
+
+import re
+
+class BibTex:
+  def __init__(self,fn):
+    self.fn = fn
+    self.parse()
+  
+  def parse(self):
+    f = open(self.fn)
+    self.bib = []
+    entry = None
+    
+    for line in f:
+      if re.search('@article',line) != None:
+        if entry != None:
+          self.bib.append(entry)
+        entry = {}
+      else:
+        c = line.split('=')
+        if len(c) >= 2 and entry != None:
+          entry[c[0].strip().strip(',{}')] = c[1].strip().strip(',{}')
+    
+    self.bib.append(entry)
+    # print 'Parsed %d entries in %s'%(len(self.bib),self.fn)
+
+if __name__ == "__main__":
+  import sys
+  bib = BibTex(sys.argv[1])
+
---
+++ compareabstract.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+compareabstract.py
+
+Created by Dan F-M on 2011-01-16.
+"""
+
+import bibparser
+import sys
+import re
+
+import numpy as np
+
+ignore_list = ['and','a','the','then','we','by','be','et','al','not']
+strip_chars = '.,(){}`\''
+
+def main():
+  bib = bibparser.BibTex(sys.argv[1])
+  aid = 373
+  
+  q_vec0 = sorted(bib.bib[aid]['Abstract'].split())
+  
+  q_vec = []
+  q_val  = []
+  for w in q_vec0:
+    w = w.strip(strip_chars).lower()
+    if (w in ignore_list) == False \
+        and re.search('\\\\',w) == None:
+      if (w in q_vec) == False:
+        q_vec.append(w)
+        q_val.append(1)
+      else:
+        q_val[-1] += 1
+  
+  q_val = np.array(q_val)/np.sqrt(np.dot(q_val,q_val))
+  prob = np.zeros(len(bib.bib))
+  
+  for ind,entry in enumerate(bib.bib):
+    if ind != aid and 'Abstract' in bib.bib[ind].keys():
+      r_vec = sorted(bib.bib[ind]['Abstract'].split())
+      r_val = np.zeros(len(q_val))
+      for w in r_vec:
+        w = w.strip(strip_chars).lower()
+        if w in q_vec:
+          r_val[q_vec.index(w)] += 1
+      mod = np.dot(r_val,r_val)
+      if mod > 0:
+        prob[ind] = np.dot(r_val/np.sqrt(mod),q_val)
+  
+  # sort based on probability (best first)
+  inds_sort = np.argsort(prob)[::-1]
+  
+  print 'Similar papers to:\n\t%s\n\t\tby: %s\n'%(bib.bib[aid]['Title'],bib.bib[aid]['Author'])
+  for i in range(10):
+    best = inds_sort[i]
+    print '%3d. (%3d)\t%s\n\t\tby: %s\n'%(i+1,best,bib.bib[best]['Title'],bib.bib[best]['Author'])
+
+if __name__ == '__main__':
+  main()
+
