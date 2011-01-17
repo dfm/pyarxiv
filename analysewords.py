8e17dfba5e5ea985c2e068ebd2e9d1b90c847842
Micha Gorelick @ sol <gorelick@nyu.edu>
2011-01-17 14:32:16 -0500

    Created analysewords.py which uses the natural language toolkit (NLTK) to extract nouns from the abstract.  Also created compareabstract-nltk which does the same as compareabstract.py but again using the noun extraction.

---
+++ analysewords.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+
+import bibparser
+from Stemmer import Stemmer
+import nltk
+import cPickle
+import sys
+import operator
+try:
+  import pytools
+except:
+  pytools = False
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print "Usage: ./%s [input.bib] [output.dat]"%sys.argv[0]
+    exit(127)
+
+  ignore_list = "and a the then we by be et al not of in to with for that on as which these our it an is than this are have at use".split()
+  strip_chars = r"""~@.,()[]{}`\/"'=1234567890% """
+
+  bib = bibparser.BibTex(sys.argv[1])
+  stem = Stemmer("english")
+
+  wordvector = {}
+  if pytools: progress = pytools.ProgressBar("Analysing",len(bib.bib))
+  for item in bib.bib:
+    if item.has_key("Abstract"):
+      abstract = nltk.wordpunct_tokenize(item["Abstract"])
+      for word in [x[0] for x in nltk.pos_tag(abstract) if x[1] in ("NN")]:
+        word = stem.stemWord(word.strip(strip_chars).lower())
+        if len(word)>1 and word not in ignore_list:
+          try:
+            wordvector[word] += 1
+          except KeyError:
+            wordvector[word] = 1
+    if pytools: progress.progress()
+  sortedwordvector = sorted(wordvector.iteritems(), key=operator.itemgetter(1))
+
+  cPickle.dump(sortedwordvector,open(sys.argv[2],"w+"))
+  print "Top 50 words:"
+  for word, count in sortedwordvector[-50:]:
+    print "%-15s %d"%(word,count)
---
+++ compareabstract-nltk.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+compareabstract.py
+
+Created by Dan F-M on 2011-01-16.
+"""
+
+import bibparser
+import sys
+import re
+
+import Stemmer
+import nltk
+
+import numpy as np
+np.random.seed()
+
+ignore_list = "and a the then we by be et al not of in to with for that on as which these our it an is than this are have at use".split()
+strip_chars = r"""~@.,()[]{}`\/"'=1234567890% """
+
+def main():
+  stem = Stemmer.Stemmer("english")
+  bib = bibparser.BibTex(sys.argv[1])
+  aid = 1#np.random.randint(len(bib.bib))
+  while ('Abstract' in bib.bib[aid].keys()) == False:
+    aid = np.random.randint(len(bib.bib))
+  
+  abstract = nltk.wordpunct_tokenize(bib.bib[aid]['Abstract'])
+  q_vec0 = sorted([x[0] for x in nltk.pos_tag(abstract) if x[1] in ("NN")])
+  
+  q_vec = []
+  q_val  = []
+  for w in q_vec0:
+    w = stem.stemWord(w.strip(strip_chars).lower())
+    if len(w)>2 and w not in ignore_list and re.search('\\\\',w) == None:
+      if (w in q_vec) == False:
+        q_vec.append(w)
+        q_val.append(1)
+      else:
+        q_val[-1] += 1
+  
+  q_val = np.array(q_val)/np.sqrt(np.dot(q_val,q_val))
+  prob = np.zeros(len(bib.bib))
+  
+  for ind,entry in enumerate(bib.bib):
+    if ind != aid and ('Abstract' in bib.bib[ind].keys()):
+      abstract = nltk.wordpunct_tokenize(bib.bib[ind]['Abstract'])
+      r_vec = sorted([x[0] for x in nltk.pos_tag(abstract) if x[1] in ("NN")])
+      r_val = np.zeros(len(q_val))
+      for w in r_vec:
+        #w = stem.stemWord(w.strip(strip_chars).lower())
+        w = w.strip(strip_chars).lower()
+        if w in q_vec:
+          r_val[q_vec.index(w)] += 1
+      mod = np.dot(r_val,r_val)
+      if mod > 0:
+        prob[ind] = np.dot(r_val/np.sqrt(mod),q_val)
+  
+  # sort based on probability (best first)
+  inds_sort = np.argsort(prob)[::-1]
+  
+  print 'Similar papers to:\n\t%s\n\t\tby: %s\n'%(bib.bib[aid]['Title'],bib.bib[aid]['Author'])
+  for i in range(10):
+    best = inds_sort[i]
+    print '%3d.\t%s\n\t\tby: %s\n\t\tid = %3d, prob = %f\n'%(i+1,bib.bib[best]['Title'],bib.bib[best]['Author'],best,prob[best])
+
+if __name__ == '__main__':
+  main()
+
