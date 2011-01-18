c6b3511496d2c764e6f685b52c2a6a0f609f5bf2
Dan F-M <dan@danfm.ca>
2011-01-18 14:07:24 -0500

    LICENSE

---
+++ README.md
@@ -0,0 +1,14 @@
+#pyarxiv
+
+##NOTE
+
+This is pre-alpha software.  Use at your own risk.
+
+##LICENSE
+
+Nominally released under GPL version 2 (see LICENSE).
+
+##AUTHORS
+
+Micha Gorelick
+Daniel Foreman-Mackey
--- compareabstract.py
+++ compareabstract.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
+#
+# Copyright 2011 Daniel Foreman-Mackey and Michael Gorelick
+# 
+# This is part of pyarxiv.
+# 
+# pyarxiv is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# 
+# pyarxiv is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with pyarxiv.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 """
 compareabstract.py
 
